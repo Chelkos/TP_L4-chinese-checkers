fc236f4ca831667d97de7c8143e1ecf12fa5760a
kacper <kacperkrasnal@gmail.com>
2020-12-10 23:11:32 +0100

    Added custom exception classes

---
+++ Trylma/src/main/java/exceptions/IllegalMoveException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class IllegalMoveException extends Exception {
+	//You cannot move like this!
+}
---
+++ Trylma/src/main/java/exceptions/LeaveBaseException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class LeaveBaseException extends Exception {
+	//You cannot leave base!
+}
