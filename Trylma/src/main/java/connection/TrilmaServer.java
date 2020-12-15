a17f740ecfc0b048584cf5b6b2d0c81d8cae0594
kacper <kacperkrasnal@gmail.com>
2020-12-15 23:11:23 +0100

    Added junit and mockito dependencies

--- Trylma/src/main/java/connection/TrilmaServer.java
+++ Trylma/src/main/java/connection/TrilmaServer.java
@@ -1,5 +1,9 @@
 package connection;
 
+import java.net.ServerSocket;
+import java.util.concurrent.Executors;
+import java.awt.Color;
+
 public class TrilmaServer {
 	public static void main(String args[]) throws Exception{
 		
