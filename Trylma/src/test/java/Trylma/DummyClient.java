6062b763e52482dfcef60b9135f32f33f39ec705
Chelkos <piotr.zuk.pl@gmail.com>
2020-12-29 18:34:59 +0100

    Upgraded DummyClient, implemented Selection tests

--- Trylma/src/test/java/Trylma/DummyClient.java
+++ Trylma/src/test/java/Trylma/DummyClient.java
@@ -5,16 +5,20 @@ import java.net.Socket;
 import java.util.Scanner;
 
 import connection.ITrilmaClient;
+import gameobjects.Board;
 
 public class DummyClient implements ITrilmaClient{
 	public Socket socket;
 	public Scanner input;
 	public PrintWriter output;
+	public Board board;
 	
 	public DummyClient() throws Exception{
 		socket=new Socket("127.0.0.1", 58901);
 		input=new Scanner(socket.getInputStream());
 		output=new PrintWriter(socket.getOutputStream(), true);
+		board = new Board();
+		board.fillBoard(2);
 	}
 	
 	public void play() throws Exception{
--- Trylma/src/test/java/Trylma/GameRulesTest.java
+++ Trylma/src/test/java/Trylma/GameRulesTest.java
@@ -1,14 +1,40 @@
 package Trylma;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.*;
 import org.mockito.Mockito;
+
+import connection.TrilmaClient;
+import exceptions.InvalidSelectException;
 
 //checking server's response for each action
 public class GameRulesTest {
 
 	@Test
 	public void invalidSelectionTest() { //not your peg, empty field
-		
+		try {
+			
+		DummyClient client1 = new DummyClient();
+		DummyClient client2 = new DummyClient();
+		client1.board.fillBoard(2);
+		String response =  client1.input.nextLine();
+		response = client1.input.nextLine();
+		assertEquals(response,"PLAYERS: 2");
+		response = client1.input.nextLine();
+		response = client1.input.nextLine();
+		assertEquals(response,"MESSAGE Your move");
+		client1.output.println("SELECT 6|6");
+		response = client1.input.nextLine();
+		assertEquals(response,"INVALID_SELECTION This field is empty!");
+		client1.output.println("SELECT 4|0");
+		response = client1.input.nextLine();
+		assertEquals(response,"VALID_SELECTION");
+		}
+		catch(Exception e)
+		{
+			assertEquals(1, 2);
+		}
 	}
 	
 	@Test
