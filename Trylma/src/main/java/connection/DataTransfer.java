741b3e3f36d3b944e6e8ba3ae1e63431a2f192da
Chelkos <piotr.zuk.pl@gmail.com>
2021-01-22 23:35:28 +0100

    Minor changes to DataTransfer

--- Trylma/src/main/java/connection/DataTransfer.java
+++ Trylma/src/main/java/connection/DataTransfer.java
@@ -12,13 +12,11 @@ public class DataTransfer {
 	      this.dataSource = dataSource;
 	      this.jdbcTemplateObject = new JdbcTemplate(dataSource);
 	   }
-	public void addNewMove(int game_ID,int peg_ID, int X, int Y,Color color) {
-		String SQL = "INSERT INTO movementlog(game_ID,peg_ID,X,Y,color) VALUES (?, ?, ?, ?, ?, ?)";
-		jdbcTemplateObject.update(SQL, game_ID,peg_ID,X,Y,color.toString());
-		SQL = "DELETE FROM lastmovementlog WHERE peg_ID = ?";
-		jdbcTemplateObject.update(SQL,peg_ID);
-		SQL = "INSERT INTO lastmovementlog(game_ID,peg_ID,X,Y,color) VALUES (?, ?, ?, ?, ?, ?)";
-		jdbcTemplateObject.update(SQL, game_ID,peg_ID,X,Y,color.toString());
+	public void addNewMove(int game_ID,int endX, int endY, int begX, int begY,Color color) {
+		String SQL = "INSERT INTO movementlog(game_ID,endX,endY,begX,begY,color) VALUES (?, ?, ?, ?, ?, ?)";
+		jdbcTemplateObject.update(SQL, game_ID,endX,endY,begX,begY,color.toString());
+		SQL = "UPDATE TABLE currentposition SET X = ?, Y = ? WHERE X = ? AND Y = ?";
+		jdbcTemplateObject.update(SQL,endX,endY,begX,begY);
 	}
 	
 }
