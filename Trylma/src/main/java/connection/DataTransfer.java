85f4cb48813cfbb703c31e21a9f1cc04f7cd6d38
Chelkos <piotr.zuk.pl@gmail.com>
2021-01-23 22:48:00 +0100

    Removed ASPECTS

--- Trylma/src/main/java/connection/DataTransfer.java
+++ Trylma/src/main/java/connection/DataTransfer.java
@@ -7,18 +7,7 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import connection.Game.Player;
-import gameobjects.Board;
 import gameobjects.Peg;
-
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Around;
-
-//@Aspect
 public class DataTransfer {
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplateObject;
@@ -32,7 +21,7 @@ public class DataTransfer {
     {	int game_ID;
     	String SQL = "SELECT MAX(game_ID) FROM movementlog;";
     	try {
-    	game_ID = jdbcTemplateObject.queryForObject(SQL,new Object[] {},Integer.class);
+    	game_ID = jdbcTemplateObject.queryForObject(SQL,Integer.class);
     	game_ID++;
     	}
     	catch(NullPointerException e)
@@ -41,17 +30,14 @@ public class DataTransfer {
     	}
     	return game_ID;
     }
- //   @AfterReturning("connection.Game.DefaultTrilmaInterface.move() && args(begI, begJ, endI, endJ, player)")
-    public void addMovementToList(int begI, int begJ, int endI, int endJ, Player player) {
+   public void addMovementToList(int begI, int begJ, int endI, int endJ, Player player) {
     	if(movementList==null)
     		movementList=new ArrayList<String>();
     	String movement=player.gameID + " " + player.name + " " + endI + " " + endJ + " " + begI + " " + begJ;
     	movementList.add(movement);
     	System.out.println(movement);
     }
-//    @AfterReturning("connection.Game.DefaultTrilmaInterface.setup() && args(gameID,board)")
-    public void setupStartingPegPosition(int game_ID,Peg[][] board){
-    	
+  public void setupStartingPegPosition(int game_ID,Peg[][] board){
     	for(int i=0; i<=16;i++)
     		for(int j=0; j<=16;j++)
     		{
